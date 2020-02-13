0950547c63264efa9d6671cce62dd35ee9a31f7c
Chris Wilcoxson <chris@slushman.com>
2020-02-12 22:34:15 -0700

    Initial Commit

---
+++ Todo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo
+//
+//  Created by Chris Wilcoxson on 2/12/20.
+//  Copyright © 2020 Chris Wilcoxson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
